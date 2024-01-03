9ea83c43f5ad6cffeb0a1280121ed6992c224e70
aihassan1 <abdosruor57@gmail.com>
2024-01-03 14:41:24 +0200

    0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,11 +13,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent == NULL)
-	{
-		return (new_node);
-	}
-
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
