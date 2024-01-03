34afb2d1fd22dc206735a955b5dcf91fe21ece5c
aihassan1 <abdosruor57@gmail.com>
2024-01-03 17:17:41 +0200

    111

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -19,7 +19,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 
 	if (parent->left != NULL)
 	{
