ce808aab653dcc5267be51458ad6270fbeef91cd
aihassan1 <abdosruor57@gmail.com>
2024-01-04 09:47:13 +0200

    7-b

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -5,13 +5,18 @@
  *
  * @tree: Pointer to the root node of the tree
  * @func: Pointer to a function to call for each node
+ *
+ * Description: This function traverses a binary tree using pre-order
+ * traversal and calls a provided function on each node.
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree == NULL || func == NULL)
+	{
 		return;
+	}
 
-	func(tree->n);
 	binary_tree_preorder(tree->left, func);
+	func(tree->n);
 	binary_tree_preorder(tree->right, func);
 }
