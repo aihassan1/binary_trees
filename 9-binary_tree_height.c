cb2da5affaaa1612c25c3cd0be511c269f1f2bf7
aihassan1 <abdosruor57@gmail.com>
2024-01-04 11:17:21 +0200

    9-

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,25 +8,37 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height = 0;
+	size_t left_height, right_height = 0;
 
 	if (tree == NULL)
-		return (-1);
-
-	left_height = binary_tree_height(tree->left);
-
-	right_height = binary_tree_height(tree->right);
-
-	/*printf("left = %d, right= %d\n", left_height, right_height);*/
-
-	if (left_height > right_height)
 	{
-
-		return (left_height + 1);
+		return (0);
 	}
 	else
 	{
 
-		return (right_height + 1);
+		/* left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0; */
+
+		if (tree->left)
+		{
+			left_height = binary_tree_height(tree->left) + 1;
+		}
+		else
+		{
+			left_height = 0;
+		}
+
+		right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+		if (left_height > right_height)
+		{
+
+			return (left_height);
+		}
+		else
+		{
+
+			return (right_height);
+		}
 	}
 }
