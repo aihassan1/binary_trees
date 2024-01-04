bbcc4dddb915780e49069bde9487d054c4fb1b20
aihassan1 <abdosruor57@gmail.com>
2024-01-04 18:31:14 +0200

    12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -8,19 +8,14 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t counter = 0;
-
 	if (tree == NULL)
 	{
 		return (0);
 	}
 
-	binary_tree_leaves(tree->left);
-	binary_tree_leaves(tree->right);
-
-	if (tree->left != NULL && tree->right != NULL)
+	if (tree->left == NULL && tree->right == NULL)
 	{
-		counter++;
+		return (1);
 	}
-	return (counter + 1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
